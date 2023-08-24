f023d845921c674564fd4da97df50973c533cac6
pasinj <pasin24j@gmail.com>
2023-08-24 18:13:53 +0700

    chore: add tailwind surface colors

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -68,7 +68,14 @@ export default {
       },
       outline: 'var(--outline-color)',
       background: 'rgba(var(--background-color), <alpha-value>)',
-      surface: 'rgba(var(--surface-color), <alpha-value>)',
+      surface: {
+        DEFAULT: 'color-mix(in srgb, rgb(var(--primary-color)) 2%, rgb(var(--background-color)))',
+        1: 'color-mix(in srgb, rgb(var(--primary-color)) 5%, rgb(var(--background-color)))',
+        2: 'color-mix(in srgb, rgb(var(--primary-color)) 8%, rgb(var(--background-color)))',
+        3: 'color-mix(in srgb, rgb(var(--primary-color)) 11%, rgb(var(--background-color)))',
+        4: 'color-mix(in srgb, rgb(var(--primary-color)) 12%, rgb(var(--background-color)))',
+        5: 'color-mix(in srgb, rgb(var(--primary-color)) 14%, rgb(var(--background-color)))',
+      },
     }),
     extend: {
       boxShadow: { ...mergeAll(muiTheme.shadows.map((v, i) => ({ [i.toString()]: v }))) },
