6cef08df64cf79eac5e65af9a8e6a1448efd9772
pasinj <pasin24j@gmail.com>
2023-09-01 10:57:10 +0700

    chore: add eslint-security

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -34,6 +34,7 @@ module.exports = {
         'plugin:import/typescript',
         'plugin:fp-ts/recommended',
         'plugin:fp-ts/recommended-requiring-type-checking',
+        'plugin:security/recommended',
       ],
       plugins: ['import'],
       rules: {
