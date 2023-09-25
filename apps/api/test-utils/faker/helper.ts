d4530c8d92853b4972eb6b806dac5c8882fe3a4c
pasinj <pasin24j@gmail.com>
2023-09-25 09:28:05 +0700

    test(api): modify returned type of generate array to readonly non-empty

--- apps/api/test-utils/faker/helper.ts
+++ apps/api/test-utils/faker/helper.ts
@@ -1,3 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { ReadonlyNonEmptyArray } from 'fp-ts/lib/ReadonlyNonEmptyArray.js';
 
-export const generateArrayOf = <T>(fn: () => T, length = 3) => faker.helpers.multiple(fn, { count: length });
+export const generateArrayOf = <T>(fn: () => T, length = 3) =>
+  faker.helpers.multiple(fn, { count: length }) as unknown as ReadonlyNonEmptyArray<T>;
