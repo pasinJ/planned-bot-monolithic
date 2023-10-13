91eb489fe9528e004539784be63e7e72ffd35548
pasinj <pasin24j@gmail.com>
2023-10-13 15:09:43 +0700

    refactor(api): refactor strategy executor service

---
+++ apps/api/src/features/shared/strategyExecutor/service.itest.ts
@@ -0,0 +1,21 @@
+import { wrapLogger } from '#infra/logging.js';
+import { executeT } from '#shared/utils/fp.js';
+
+import { getStrategyExecutorConfig } from './config.js';
+import { startStrategyExecutor } from './service.js';
+
+describe('UUT: Start strategy executor', () => {
+  describe('[WHEN] start strategy executor', () => {
+    it('[THEN] it will return Right of strategy executor', async () => {
+      const deps = {
+        isolatedConsole: console,
+        loggerIo: wrapLogger(console),
+        getConfig: getStrategyExecutorConfig,
+      };
+
+      const result = await executeT(startStrategyExecutor(deps));
+
+      expect(result).toEqualRight(expect.toContainAllKeys(['execute', 'stop']));
+    });
+  });
+});
