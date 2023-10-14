0fe5d8d76240b6ed46f2c26dae0c1e4fb5eaaef7
pasinj <pasin24j@gmail.com>
2023-10-14 15:56:25 +0700

    feat(api): implement calculate win/loss metrics  function

--- apps/api/src/features/shared/strategyPerformance.test.ts
+++ apps/api/src/features/shared/strategyPerformance.test.ts
@@ -1,5 +1,6 @@
 import { mockKline } from '#test-utils/features/shared/kline.js';
 import { mockFilledMarketOrder } from '#test-utils/features/shared/order.js';
+import { mockClosedTrade } from '#test-utils/features/shared/trades.js';
 
 import { FilledOrder } from './order.js';
 import { InitialCapital } from './strategy.js';
@@ -7,8 +8,10 @@ import { Return } from './strategyExecutorModules/strategy.js';
 import {
   calculateBuyAndHoldReturn,
   calculateRateOfInvestment,
+  calculateWinLossMetrics,
   getTotalTradeVolume,
 } from './strategyPerformance.js';
+import { ClosedTrade, NetReturn } from './trade.js';
 
 describe('UUT: Calculate buy and hold return', () => {
   describe('[GIVEN] there is no filled entry order', () => {
@@ -92,6 +95,187 @@ describe('UUT: Get total trade volume', () => {
         const result = getTotalTradeVolume(filledOrders);
 
         expect(result).toBe(8);
+      });
+    });
+  });
+});
+
+describe('UUT: Calculate win/loss metrics', () => {
+  const entryWithNoFeeBase = { orderSide: 'ENTRY', fee: { amount: 0 } } as const;
+  const exitWithNoFeeBase = { orderSide: 'EXIT', fee: { amount: 0 } } as const;
+  const winningTrade = mockClosedTrade(
+    mockFilledMarketOrder({ ...entryWithNoFeeBase, quantity: 1, filledPrice: 3 }),
+    mockFilledMarketOrder({ ...exitWithNoFeeBase, quantity: 1, filledPrice: 4 }),
+  );
+  const losingTrade = mockClosedTrade(
+    mockFilledMarketOrder({ ...entryWithNoFeeBase, quantity: 1, filledPrice: 3 }),
+    mockFilledMarketOrder({ ...exitWithNoFeeBase, quantity: 1, filledPrice: 2 }),
+  );
+  const evenTrade = mockClosedTrade(
+    mockFilledMarketOrder({ ...entryWithNoFeeBase, quantity: 1, filledPrice: 3 }),
+    mockFilledMarketOrder({ ...exitWithNoFeeBase, quantity: 1, filledPrice: 3 }),
+  );
+
+  describe('[GIVEN] the closed trade list is empty', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return correct metrics', () => {
+        const closedTrades = [] as ClosedTrade[];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toEqual({
+          numOfTotalTrades: 0,
+          numOfWinningTrades: 0,
+          numOfLosingTrades: 0,
+          numOfEvenTrades: 0,
+          winRate: 0,
+          lossRate: 0,
+          evenRate: 0,
+          avgProfit: 0,
+          avgLoss: 0,
+          largestProfit: 0,
+          largestLoss: 0,
+        });
+      });
+    });
+  });
+
+  describe('[GIVEN] there are 3 trades in the closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return number of total trades equal to 3', () => {
+        const closedTrades = [winningTrade, losingTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('numOfTotalTrades', 3);
+      });
+    });
+  });
+  describe('[GIVEN] there are 2 winning trades in the closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return number of winning trades equal to 2', () => {
+        const closedTrades = [winningTrade, winningTrade, losingTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('numOfWinningTrades', 2);
+      });
+    });
+  });
+  describe('[GIVEN] there are 2 losing trades in the closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return number of losing trades equal to 2', () => {
+        const closedTrades = [winningTrade, losingTrade, losingTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('numOfLosingTrades', 2);
+      });
+    });
+  });
+  describe('[GIVEN] there are 1 even trades in the closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return number of even trades equal to 1', () => {
+        const closedTrades = [winningTrade, losingTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('numOfEvenTrades', 1);
+      });
+    });
+  });
+
+  describe('[GIVEN] there are 2 winning trades in the 5 closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return win rate equal to 40', () => {
+        const closedTrades = [winningTrade, winningTrade, losingTrade, evenTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('winRate', 40);
+      });
+    });
+  });
+  describe('[GIVEN] there are 1 losing trades in the 5 closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return loss rate equal to 20', () => {
+        const closedTrades = [winningTrade, winningTrade, losingTrade, evenTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('lossRate', 20);
+      });
+    });
+  });
+  describe('[GIVEN] there are 3 even trades in the 5 closed trades list', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return loss rate equal to 60', () => {
+        const closedTrades = [winningTrade, losingTrade, evenTrade, evenTrade, evenTrade];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('evenRate', 60);
+      });
+    });
+  });
+
+  describe('[GIVEN] there are 2 winning trades with 10 and 20 net returns', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return average profit equal to 15', () => {
+        const closedTrades = [
+          { ...winningTrade, netReturn: 10 as NetReturn },
+          { ...winningTrade, netReturn: 20 as NetReturn },
+        ];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('avgProfit', 15);
+      });
+    });
+  });
+  describe('[GIVEN] there are 3 losing trades with -10, -20, and -30 net returns', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return average loss equal to -20', () => {
+        const closedTrades = [
+          { ...losingTrade, netReturn: -10 as NetReturn },
+          { ...losingTrade, netReturn: -20 as NetReturn },
+          { ...losingTrade, netReturn: -30 as NetReturn },
+        ];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('avgLoss', -20);
+      });
+    });
+  });
+
+  describe('[GIVEN] there are 3 winning trades with 10, 20, and 30 net returns', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return largest profit equal to 30', () => {
+        const closedTrades = [
+          { ...winningTrade, netReturn: 20 as NetReturn },
+          { ...winningTrade, netReturn: 30 as NetReturn },
+          { ...winningTrade, netReturn: 10 as NetReturn },
+        ];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('largestProfit', 30);
+      });
+    });
+  });
+  describe('[GIVEN] there are 3 losing trades with -5, -10, and -20 net returns', () => {
+    describe('[WHEN] calculate win/loss metrics', () => {
+      it('[THEN] it will return largest loss equal to -20', () => {
+        const closedTrades = [
+          { ...losingTrade, netReturn: -10 as NetReturn },
+          { ...losingTrade, netReturn: -20 as NetReturn },
+          { ...losingTrade, netReturn: -5 as NetReturn },
+        ];
+
+        const result = calculateWinLossMetrics(closedTrades);
+
+        expect(result).toHaveProperty('largestLoss', -20);
       });
     });
   });
--- apps/api/src/features/shared/strategyPerformance.ts
+++ apps/api/src/features/shared/strategyPerformance.ts
@@ -1,13 +1,16 @@
 import { Decimal } from 'decimal.js';
-import { propEq } from 'ramda';
+import { max, min, propEq } from 'ramda';
+import { DeepReadonly } from 'ts-essentials';
 import { z } from 'zod';
 
 import { isUndefined } from '#shared/utils/general.js';
+import { to2DigitDecimalNumber, to8DigitDecimalNumber } from '#shared/utils/number.js';
 
 import { Kline } from './kline.js';
 import { FilledOrder } from './order.js';
 import { InitialCapital } from './strategy.js';
 import { Return } from './strategyExecutorModules/strategy.js';
+import { ClosedTrade } from './trade.js';
 
 type BuyAndHoldReturn = number & z.BRAND<'BuyAndHoldReturn'>;
 export function calculateBuyAndHoldReturn(
@@ -52,4 +55,98 @@ export function getTotalTradeVolume(filledOrders: readonly FilledOrder[]): Total
       .reduce((sum, order) => sum.plus(order.quantity), new Decimal(0))
       .toDecimalPlaces(8, Decimal.ROUND_HALF_UP)
       .toNumber() as TotalTradeVolume;
+}
+
+type WinLossMetrics = DeepReadonly<{
+  numOfTotalTrades: NumOfTotalTrades;
+  numOfWinningTrades: NumOfWinningTrades;
+  numOfLosingTrades: NumOfLosingTrades;
+  numOfEvenTrades: NumOfEvenTrades;
+  winRate: WinRate;
+  lossRate: LossRate;
+  evenRate: EvenRate;
+  avgProfit: AvgProfit;
+  avgLoss: AvgLoss;
+  largestProfit: LargestProfit;
+  largestLoss: LargestLoss;
+}>;
+type NumOfTotalTrades = number & z.BRAND<'NumOfTotalTrades'>;
+type NumOfWinningTrades = number & z.BRAND<'NumOfWinningTrades'>;
+type NumOfLosingTrades = number & z.BRAND<'NumOfLosingTrades'>;
+type NumOfEvenTrades = number & z.BRAND<'NumOfEvenTrades'>;
+type WinRate = number & z.BRAND<'WinRate'>;
+type LossRate = number & z.BRAND<'LossRate'>;
+type EvenRate = number & z.BRAND<'EvenRate'>;
+type AvgProfit = number & z.BRAND<'AvgProfit'>;
+type AvgLoss = number & z.BRAND<'AvgLoss'>;
+type LargestProfit = number & z.BRAND<'LargestProfit'>;
+type LargestLoss = number & z.BRAND<'LargestLoss'>;
+export function calculateWinLossMetrics(closedTrades: readonly ClosedTrade[]): WinLossMetrics {
+  const {
+    numOfTotalTrades,
+    numOfWinningTrades,
+    numOfLosingTrades,
+    numOfEvenTrades,
+    totalProfit,
+    totalLoss,
+    largestProfit,
+    largestLoss,
+  } = closedTrades.reduce(
+    (prev, trade) => {
+      if (trade.netReturn > 0) {
+        return {
+          ...prev,
+          numOfTotalTrades: prev.numOfTotalTrades + 1,
+          numOfWinningTrades: prev.numOfWinningTrades + 1,
+          totalProfit: prev.totalProfit.plus(trade.netReturn),
+          largestProfit: max(prev.largestProfit, trade.netReturn),
+        };
+      } else if (trade.netReturn < 0) {
+        return {
+          ...prev,
+          numOfTotalTrades: prev.numOfTotalTrades + 1,
+          numOfLosingTrades: prev.numOfLosingTrades + 1,
+          totalLoss: prev.totalLoss.plus(trade.netReturn),
+          largestLoss: min(prev.largestLoss, trade.netReturn),
+        };
+      } else {
+        return {
+          ...prev,
+          numOfTotalTrades: prev.numOfTotalTrades + 1,
+          numOfEvenTrades: prev.numOfEvenTrades + 1,
+        };
+      }
+    },
+    {
+      numOfTotalTrades: 0,
+      numOfWinningTrades: 0,
+      numOfLosingTrades: 0,
+      numOfEvenTrades: 0,
+      totalProfit: new Decimal(0),
+      totalLoss: new Decimal(0),
+      largestProfit: 0,
+      largestLoss: 0,
+    },
+  );
+
+  const winRate = new Decimal(numOfWinningTrades).times(100).dividedBy(numOfTotalTrades);
+  const lossRate = new Decimal(numOfLosingTrades).times(100).dividedBy(numOfTotalTrades);
+  const evenRate = new Decimal(numOfEvenTrades).times(100).dividedBy(numOfTotalTrades);
+
+  const avgProfit = totalProfit.dividedBy(numOfWinningTrades);
+  const avgLoss = totalLoss.dividedBy(numOfLosingTrades);
+
+  return {
+    numOfTotalTrades,
+    numOfWinningTrades,
+    numOfLosingTrades,
+    numOfEvenTrades,
+    winRate: winRate.isNaN() ? 0 : to2DigitDecimalNumber(winRate),
+    lossRate: lossRate.isNaN() ? 0 : to2DigitDecimalNumber(lossRate),
+    evenRate: evenRate.isNaN() ? 0 : to2DigitDecimalNumber(evenRate),
+    avgProfit: avgProfit.isNaN() ? 0 : to8DigitDecimalNumber(avgProfit),
+    avgLoss: avgLoss.isNaN() ? 0 : to8DigitDecimalNumber(avgLoss),
+    largestProfit,
+    largestLoss,
+  } as WinLossMetrics;
 }
