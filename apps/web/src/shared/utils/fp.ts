4af8ee3e8924573b8ea8b9f4d884902091a52e15
pasinj <pasin24j@gmail.com>
2023-10-27 11:38:18 +0700

    chore(web): implement wrapInIo util function

--- apps/web/src/shared/utils/fp.ts
+++ apps/web/src/shared/utils/fp.ts
@@ -1,7 +1,9 @@
 import { pass } from 'fp-ts-std/IO';
 import { execute as executeT } from 'fp-ts-std/Task';
 import * as e from 'fp-ts/lib/Either';
+import * as io from 'fp-ts/lib/IO';
 import * as te from 'fp-ts/lib/TaskEither';
+import { AnyFunction } from 'ts-essentials';
 
 export { execute as executeT } from 'fp-ts-std/Task';
 export { execute as executeIo } from 'fp-ts-std/IO';
@@ -16,3 +18,9 @@ export async function executeTeToPromise<E, A>(input: te.TaskEither<E, A>): Prom
 }
 
 export const ioVoid = pass;
+export function wrapInIo<F extends AnyFunction>(fn: F): (...args: Parameters<F>) => io.IO<ReturnType<F>> {
+  return (...args) =>
+    () =>
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      fn(...args);
+}
