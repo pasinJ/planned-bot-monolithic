4c9ec8cfaa10be0a3c37e39d9351aafbc87ad70c
pasinj <pasin24j@gmail.com>
2023-11-05 15:26:05 +0700

    fix(web): disable-toast

--- apps/web/src/state/ServerStateProvider.context.tsx
+++ apps/web/src/state/ServerStateProvider.context.tsx
@@ -1,22 +1,21 @@
-import { Query, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { is } from 'ramda';
 import { PropsWithChildren } from 'react';
-import { toast } from 'react-toastify';
 
 const queryClient = new QueryClient({
-  queryCache: new QueryCache({ onError: onErrorQuery }),
+  queryCache: new QueryCache(),
+  // queryCache: new QueryCache({ onError: onErrorQuery }),
 });
 
-function onErrorQuery(error: unknown, query: Query<unknown, unknown, unknown>): void {
-  const transformedError = is(String, error)
-    ? error
-    : error instanceof Error
-    ? `[${error.name}] ${error.message}`
-    : JSON.stringify(error);
+// function onErrorQuery(error: unknown, query: Query<unknown, unknown, unknown>): void {
+//   const transformedError = is(String, error)
+//     ? error
+//     : error instanceof Error
+//     ? `[${error.name}] ${error.message}`
+//     : JSON.stringify(error);
 
-  if (query.state.data !== undefined) toast.error(`Something went wrong: ${transformedError}`);
-}
+//   if (query.state.data !== undefined) toast.error(`Something went wrong: ${transformedError}`);
+// }
 
 export default function ServerStateProvider({ children }: PropsWithChildren) {
   return (
